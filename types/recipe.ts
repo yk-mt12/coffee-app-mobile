6fdf6a54db88b9691785d245176d64497b2e4921
kitsune_yk <agvksl543@gmail.com>
2023-05-08 01:35:53 +0900

    type: recipeのtype定義を作成

---
+++ types/recipe.ts
@@ -0,0 +1,9 @@
+export type Recipe = {
+  name: string;
+  coffee_amount: number;
+  water_amount: number;
+  grind_size: string;
+  brew_time: number;
+  description: string;
+  image_url: string;
+}
